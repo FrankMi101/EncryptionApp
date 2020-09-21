f3dea9c1513f77e7cbf9d4f27d580bd0b20b837a
Mi <mif@tcdsb.org>
2020-09-20 20:50:37 -0400

    Setup New Project Encryption and Decryption

---
+++ SIC/Loading.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SIC
+{
+    public partial class Loading : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                string goPage = Page.Request.QueryString["pID"].ToString();
+                switch (goPage)
+                    {
+                    case "StudentList":
+                        goPage = "StudentListPage.aspx";
+                        break;
+                    case "SchoolList":
+                        goPage = "SchoolListPage.aspx";
+                        break;
+                    case "StaffListTCDSB":
+                        goPage = "StaffListPage.aspx?Scope=All";
+                        break;
+                    case "StaffListSchool":
+                        goPage = "StaffListPage.aspx?Scope=School";
+                        break;
+                    case "Encryption":
+                        goPage = "EncryptionStr.aspx";
+                        break;
+                    default:
+                        goPage = "Home.aspx";
+                        break;
+                }
+
+                PageURL.HRef = goPage;
+            }
+        }
+    }
+}
